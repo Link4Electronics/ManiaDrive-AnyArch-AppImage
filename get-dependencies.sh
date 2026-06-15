6f11c9c9950545035eafde69207b5cd5dfe0c7e4
Link4Electronics <link4electronics@gmail.com>
2026-06-15 10:54:11 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -8,7 +8,7 @@ case "$ARCH" in
 	x86_64|aarch64|armv7h|powerpc64|ppc64|powerpc|ppc|powerpc64le|ppc64le|riscv|pentium4|i686)
 		echo "Installing Arch package dependencies..."
 		echo "---------------------------------------------------------------"
-		# pacman -Syu --noconfirm PACKAGESHERE
+		pacman -Syu --noconfirm base-devel cmake mesa glu glew openal freealut libjpeg-turbo libpng zlib curl libxml2 libvorbis libogg libx11 libxinerama v4l-utils bison
 
 		#echo "Installing debloated packages..."
 		#echo "---------------------------------------------------------------"
@@ -27,9 +27,9 @@ esac
 # If the application needs to be manually built that has to be done down here
 
 # if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+mkdir -p ./AppDir/bin
+git clone https://github.com/Link4Electronics/ManiaDrive
+cd ManiaDrive
+mkdir build && cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake --build .
